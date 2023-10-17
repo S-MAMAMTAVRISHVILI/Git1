1c3b790d82e21f260c77a567f54cf1f04b0b3421
S <s>
2023-10-17 17:18:20 +0400

    {"types":"C","runs":1}

--- Problem15II.java
+++ Problem15II.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class Problem15II {
-
+public class Problem15II extends ConsoleProgram {
+	public void run() {
+		int num1 = readInt("Enter first number:");
+		int num2 = readInt("Enter second number:");
+		int avg = (num1 + num2) / 2;
+		println(avg);
+		
+		
+		
+		
+	}
 }
