78dc4716f9685f6982f4a367d11fd7f6b0df430b
S <s>
2023-10-17 17:52:38 +0400

    {"types":"C","runs":6}

--- Problem17II.java
+++ Problem17II.java
@@ -1,4 +1,45 @@
+import java.math.BigDecimal;
 
-public class Problem17II {
+import acm.program.ConsoleProgram;
+
+public class Problem17II extends ConsoleProgram {
+	public void run() {
+		
+		calculateInBigDecimal();
+		
+		
+		
+		
+	
+	}
+	private void calculateInDouble(){ 
+		int integerPart = (int)a;
+		double fractionPart = a - integerPart;
+	
+	}
+	private void calculateInBigDecimal(){
+		
+		
+		
+		
+		BigDecimal decimal = decimal.intValue();
+		BigDecimal fractionPart = decimal.remainder(BigDecimal.ONE);
+		println("Integer part: " + integerPart);
+		println("Fraction part" + fractionPart);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+
 
 }
