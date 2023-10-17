311adaf16a0f663cb760bcc4f56178e2fce67ea9
S <s>
2023-10-17 17:55:27 +0400

    {"types":"C","runs":11}

--- Problem18.java
+++ Problem18.java
@@ -4,8 +4,10 @@ public class Problem18 extends ConsoleProgram {
 	public void run() {
 		int n1 = readInt("Enter first number:");
 		int n2 = readInt("Enter second number:");
-		if((n1 - n2)>0){
+		if((n1 - n2)>=0){
 			println(n1);
+		}else{
+		println(n2);	
 		}
 	 
 	 
