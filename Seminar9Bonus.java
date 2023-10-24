ada6a6a90cb0ab82c0cbb7fd2569debb0a490022
S <s>
2023-10-24 17:59:38 +0400

    {"types":"C","runs":1}

--- Seminar9Bonus.java
+++ Seminar9Bonus.java
@@ -1,4 +1,31 @@
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
-public class Seminar9Bonus {
+public class Seminar9Bonus extends GraphicsProgram {
+	public void run() {
+		int x = 4;
+		GLabel label = new GLabel("x before method + x");
+		add(label, )
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
