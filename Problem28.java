764f0fd6a2b7018db6deaef32942f76bc14df9c1
S <s>
2023-10-26 17:22:09 +0400

    {"types":"C","runs":3}

--- Problem28.java
+++ Problem28.java
@@ -1,8 +1,19 @@
 import acm.program.ConsoleProgram;
 
 public class Problem28 extends ConsoleProgram {
+	private static final int SENTINEL = -1;
+	
+	
 	public void run() {
-		int n = readInt("Enter n: ");
+		int n = 1;
+		while(n >= 0){
+			n = readInt("Enter n: ");
+//			if(n < 0){
+//				println(checkNumber());
+//			}
+		
+		
+		}
 		
 		
 		
@@ -15,6 +26,18 @@ public class Problem28 extends ConsoleProgram {
 		
 		
 		
+		
+	}
+
+	private int checkNumber(int n) {
+		int sum = 0;
+		if(n % 2 == 0){
+			sum = sum + 1;
+		}
+			
+			
+		
+		return(sum);
 	}
 	
 	
