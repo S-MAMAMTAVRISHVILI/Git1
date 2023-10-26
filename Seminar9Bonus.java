de79b1f6928ca84915d78d73650d1d6925637863
S <s>
2023-10-26 17:07:40 +0400

    {"types":"C","runs":1}

--- Seminar9Bonus.java
+++ Seminar9Bonus.java
@@ -5,7 +5,7 @@ public class Seminar9Bonus extends GraphicsProgram {
 	public void run() {
 		int x = 4;
 		GLabel label = new GLabel("x before method + x");
-		add(label, )
+	//	add(label, )
 		
 		
 		
