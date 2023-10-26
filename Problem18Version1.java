6504c3afcc21fca5de27a82a8eed8df7fe2e7527
S <s>
2023-10-27 01:02:48 +0400

    {"types":"C","runs":4}

--- Problem18Version1.java
+++ Problem18Version1.java
@@ -9,9 +9,9 @@ public class Problem18Version1 extends ConsoleProgram {
 		int num1 = readInt("Enter number1: ");
 		int num2 = readInt("Enter number2: ");
 		if(num1 >= num2){
-			println("Max is:" + num1);
+			println("Max is: " + num1);
 		}else{
-		println("Max is:" + num2);	
+		println("Max is: " + num2);	
 		} 
 	}
 }
