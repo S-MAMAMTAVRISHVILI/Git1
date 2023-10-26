c035a5812ec7286bdee2315de2a2a2104d2ce98b
S <s>
2023-10-27 01:45:43 +0400

    {"types":"C","runs":13}

--- Problem21Version1.java
+++ Problem21Version1.java
@@ -6,14 +6,18 @@ public class Problem21Version1 extends GraphicsProgram{
 	private static final int HORIZONTALLINES = 10;
 	
 	public void run() {
+		horizontalLines();
 		verticalLines();
-		
-		
-		
-		
-		
-		
-		
+	}
+
+	private void horizontalLines() {
+		double x1 = 0;
+		double y1 = getHeight() / (HORIZONTALLINES + 1);
+		double x2 = getWidth();
+		double y2 = getHeight() / (HORIZONTALLINES + 1);
+		for(int i = 0; i <= HORIZONTALLINES; i++){
+			add(new GLine(x1, y1 * i, x2, y2 * i));
+		}
 		
 	}
 
@@ -23,7 +27,7 @@ public class Problem21Version1 extends GraphicsProgram{
 		double x2 = getWidth() / (VERTICALLINES + 1);
 		double y2 = getHeight();
 		for(int i = 0; i <= VERTICALLINES; i++){
-			add(new GLine(x1 * i, y1, x2 * i, y2  ));
+			add(new GLine(x1 * i, y1, x2 * i, y2));
 		}
 	}
 
