5a82a2f9a0e974d49ba4816b10384d4e024d149a
S <s>
2023-10-31 17:18:07 +0400

    {"types":"C","runs":8}

--- Problem29.java
+++ Problem29.java
@@ -3,13 +3,13 @@ import acm.program.ConsoleProgram;
 public class Problem29 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("Enter number: ");
-		if(n == 0){
-			println("Fibonacci sequence: " + n);
+		if(n == 1){
+			println("Fibonacci sequence: 0");
 		}else{
 		int num1 = 0;
 		int num2 = 1;
 		int sum = num1 + num2;
-		for(int i = 1; i < n; i++){
+		for(int i = 2; i < n; i++){
 			sum = num1 + num2;
 			num1 = num2;
 			num2 = sum;
