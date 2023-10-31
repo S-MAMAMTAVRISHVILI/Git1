c260fd0c2e2301c842feb193190956ac5e1e949a
S <s>
2023-10-31 17:43:20 +0400

    {"types":"C","runs":22}

--- Problem30.java
+++ Problem30.java
@@ -1,4 +1,35 @@
+import acm.program.ConsoleProgram;
 
-public class Problem30 {
+public class Problem30 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter number: ");
+		int ReversedNumber = reverseNumber(n);
+//		println(ReversedNumber);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
+	private int reverseNumber(int n) {
+		while( n != 0){
+			int x = n % 10;
+			println(x);
+			x = x / 10;
+			
+			
+			
+			
+			
+			
+		}
+		
+		return 0;
+	}
 }
