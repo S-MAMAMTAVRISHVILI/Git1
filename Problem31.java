919679355d774d7d6f8a37679280971a8e40b755
S <s>
2023-10-31 18:01:32 +0400

    {"types":"C","runs":28}

--- Problem31.java
+++ Problem31.java
@@ -1,4 +1,35 @@
+import acm.program.ConsoleProgram;
 
-public class Problem31 {
-
+public class Problem31 extends ConsoleProgram {
+	private static final int MAX_NUM = 1000;
+	public void run() {
+		int n = readInt("Enter number :");
+		for(int i = 1; i < MAX_NUM; i++) {
+			if(isPrime(i)) {
+				println(i);
+			}
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	private boolean isPrime(int i) {
+		for(int i = 2; i <= Math.sqrt(num); i++){
+		}
+			
+		return true;
+	
+	
+	
+	
 }
