2e0b58e65049d374b4195f4582b08ebc0c1d75b4
S <s>
2023-11-02 16:50:24 +0400

    {"types":"C","runs":7}

--- Problem32.java
+++ Problem32.java
@@ -1,39 +1,29 @@
 import acm.program.ConsoleProgram;
 
-
-
-
-
 public class Problem32 extends ConsoleProgram {
 	private static final int MAX_NUM = 1000;
+
 	public void run() {
-		int n = readInt("Enter number :");
-		for(int i = 1; i < MAX_NUM; i++) {
-			if(isPrime(i)) {
+
+		for (int i = 1; i < MAX_NUM; i++) {
+			if (isPrime(i)) {
 				println(i);
 			}
-			
-			
-			
-			
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
+
 	private boolean isPrime(int i) {
-		for(int j = 2; j <= Math.sqrt(i); j++){
+		for (int j = 2; j <= Math.sqrt(i); j++) {
+			if (i % j == 0) {
+				return false;
+			}
+
 		}
-			
+
 		return true;
-	
-	
+
 	}
-	
+
 }
--- practice1.java
+++ practice1.java
@@ -5,22 +5,22 @@ import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class practice1 extends GraphicsProgram {
-	
+
 	private static final double HEAD_LENGTH = 350;
 	private static final double EYE_MARGIN = 40;
-	private static final double EYE_DIAMETER = 100; 
+	private static final double EYE_DIAMETER = 100;
 	private static final double MOUTH_HEIGHT = 35;
 	private static final double MOUTH_MARGIN = 40;
 	private static final Color MOUTH_COLOR = Color.RED;
 	private static final Color LEFT_EYE_COLOR = Color.BLUE;
 	private static final Color RIGHT_EYE_COLOR = Color.GREEN;
-	
+
 	public void run() {
 		drawHead();
 		drawEyes();
 		drawMouth();
 	}
-	
+
 	private void drawMouth() {
 		GRect mouth = new GRect(HEAD_LENGTH - EYE_DIAMETER - 2 * EYE_MARGIN, MOUTH_HEIGHT);
 		mouth.setFilled(true);
@@ -29,17 +29,17 @@ public class practice1 extends GraphicsProgram {
 		double y = getHeight() / 2 + HEAD_LENGTH / 2 - MOUTH_MARGIN - MOUTH_HEIGHT;
 		add(mouth, x, y);
 	}
-	
+
 	private void drawHead() {
 		GRect head = new GRect(HEAD_LENGTH, HEAD_LENGTH);
 		add(head, getWidth() / 2 - HEAD_LENGTH / 2, getHeight() / 2 - HEAD_LENGTH / 2);
 	}
-	
+
 	private void drawEyes() {
 		drawLeftEye();
 		drawRightEye();
 	}
-	
+
 	private void drawLeftEye() {
 		double x = getWidth() / 2 - HEAD_LENGTH / 2 + EYE_MARGIN;
 		double y = getHeight() / 2 - HEAD_LENGTH / 2 + EYE_MARGIN;
@@ -48,7 +48,7 @@ public class practice1 extends GraphicsProgram {
 		eye.setColor(LEFT_EYE_COLOR);
 		add(eye, x, y);
 	}
-	
+
 	private void drawRightEye() {
 		double x = getWidth() / 2 + HEAD_LENGTH / 2 - EYE_MARGIN - EYE_DIAMETER;
 		double y = getHeight() / 2 - HEAD_LENGTH / 2 + EYE_MARGIN;
@@ -57,6 +57,5 @@ public class practice1 extends GraphicsProgram {
 		eye.setColor(RIGHT_EYE_COLOR);
 		add(eye, x, y);
 	}
-	
 
 }
