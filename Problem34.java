c2aca3b104ce9beb5a02cb10348ba8bb63ac17e9
S <s>
2023-11-02 17:26:58 +0400

    {"types":"C","runs":3}

--- Problem34.java
+++ Problem34.java
@@ -10,14 +10,14 @@ public class Problem34 extends GraphicsProgram {
 	private static final int MAX_RADIUS = 20;
 	public void run() {
 		rgen.nextInt();
-		
+		addRandomCircle();
 	}
 	
 
 
 	private void addRandomCircle() {
 		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
-		double x = rgen.nextDouble(0, getWidth()  -2 * radius);
+		double x = rgen.nextDouble(0, getWidth()  - 2 * radius);
 		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
 		Color color = rgen.nextColor();
 		
