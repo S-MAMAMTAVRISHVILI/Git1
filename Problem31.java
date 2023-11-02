f5de7153558e875ccec7cb797146fd59ab7c0a75
S <s>
2023-11-02 16:50:23 +0400

    {"types":"C","runs":7}

--- Problem31.java
+++ Problem31.java
@@ -1,42 +1,28 @@
 import acm.program.ConsoleProgram;
 
-
-
-
-
-
 public class Problem31 extends ConsoleProgram {
 	public void run() {
-		int n = readInt("Enter number: ");
+		int n = readInt("Enter number:");
 		boolean x = checkPrime(n);
-		if(x == false){
-			println("The number is not prime.");	 
-		}else{
-		println("The number is prime.");
+		if (x == false) {
+			println("The number is not prime.");
+		} else {
+			println("The number is prime.");
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-	}
 
-	
+	}
 
 	private boolean checkPrime(int n) {
 		double x = Math.sqrt(n);
-		for(int i = 2; i <= x; i++){
+		for (int i = 2; i <= x; i++) {
 			int remainder = n % i;
-			
-			if(remainder == 0){
+
+			if (remainder == 0) {
 				return false;
-				
+
 			}
-		}	
-		return true;	
-		
+		}
+		return true;
+
 	}
 }
