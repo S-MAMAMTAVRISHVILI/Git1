804db5656bd536dd3a20aca4bb3a62ebd9e99b6f
S <s>
2023-11-07 17:49:10 +0400

    {"types":"C","runs":14}

--- Problem39.java
+++ Problem39.java
@@ -1,14 +1,16 @@
 import acm.graphics.GOval;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class Problem39 extends ConsoleProgram {
+public class Problem39 extends GraphicsProgram {
 	private static final int BALL_RADIUS = 70;
 	private static final int PAUSE_TIME = 500;
 
 	public void run() {
 		GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
 		double x1 = (getWidth() - BALL_RADIUS) / 2;
-		double y2 = (getHeight() - BALL_RADIUS) / 2;
+		double y1 = (getHeight() - BALL_RADIUS) / 2;
+		add(ball, x1, y1);
 		while((ball.getX() <= getWidth() - 2 * BALL_RADIUS)) { 
 			ball.move(1, 0);
 			ball.pause(PAUSE_TIME);	
