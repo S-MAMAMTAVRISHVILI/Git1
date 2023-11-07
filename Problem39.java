3d9c3aa70d87c0fc47fbedb2eade2dc3eb0f2076
S <s>
2023-11-07 17:49:51 +0400

    {"types":"C","runs":17}

--- Problem39.java
+++ Problem39.java
@@ -4,7 +4,7 @@ import acm.program.GraphicsProgram;
 
 public class Problem39 extends GraphicsProgram {
 	private static final int BALL_RADIUS = 70;
-	private static final int PAUSE_TIME = 100;
+	private static final int PAUSE_TIME = 20;
 
 	public void run() {
 		GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
