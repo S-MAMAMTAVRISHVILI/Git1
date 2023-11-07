5dd8baa3e0fea5f3d5768ef526382e8ddded3eba
S <s>
2023-11-07 12:52:34 +0400

    {"types":"C","runs":0}

--- Problem33Version1.java
+++ Problem33Version1.java
@@ -1,4 +1,25 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Problem33Version1 {
 
+
+public class Problem33Version1 extends GraphicsProgram {
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+		double radius = rgen.nextDouble(10,200);
+		GOval oval = new GOval(2 * radius, 2 * radius);
+		oval.setFilled(true);
+		oval.setColor(rgen.nextColor());
+		add(oval, getWidth() / 2 - radius, getWidth() / 2 - radius);
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
