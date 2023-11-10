13611f2e795c40131db5ec0fc5a5f532da3e9869
S <s>
2023-11-11 00:40:32 +0400

    {"types":"C","runs":14}

--- Star.java
+++ Star.java
@@ -9,7 +9,7 @@ public class Star extends GraphicsProgram {
 		addMouseListeners();
 
 	}
-	public void MouseClicked(MouseEvent e) {
+	public void mouseClicked(MouseEvent e) {
 		GOval oval = new GOval(OVAL_SIZE, OVAL_SIZE);
 		oval.setFilled(true);
 		add(oval, e.getX(), e.getY());
