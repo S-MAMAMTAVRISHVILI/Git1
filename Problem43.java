d9363af0f357efc52efe40b0fa4c0f7c1f13aac3
S <s>
2023-11-11 01:15:04 +0400

    {"types":"C","runs":33}

--- Problem43.java
+++ Problem43.java
@@ -1,4 +1,29 @@
+import java.awt.event.MouseEvent;
 
-public class Problem43 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class Problem43 extends GraphicsProgram {
+	private GOval oval;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void init() {
+		oval = new GOval(100, 100);
+		oval.setFilled(true);
+		oval.setColor(rgen.nextColor());
+		add(oval, getWidth() - 50, getHeight() - 50);
+		
+		addMouseListeners();
+	}
+	public void mouseDragged(MouseEvent e) {
+		
+		
+		
+		
+		
+		
+	}
+
+
 
 }
