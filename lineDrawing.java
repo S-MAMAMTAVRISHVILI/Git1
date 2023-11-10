aa6131a8645f25e4263665eb076179d9045546c6
S <s>
2023-11-11 00:53:11 +0400

    {"types":"C","runs":18}

--- lineDrawing.java
+++ lineDrawing.java
@@ -1,4 +1,18 @@
+import java.awt.event.MouseEvent;
 
-public class lineDrawing {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
+
+public class lineDrawing extends GraphicsProgram {
+	private GLine line;
+
+	public void init() {
+		addMouseListeners();
+	}
+
+	public void mousePressed(MouseEvent e) {
+		GLine line = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+		add(line);
+	}
 
 }
