f58885acb921682a69fedc3cf957c6eaa32f8f7b
S <s>
2023-11-11 01:04:57 +0400

    {"types":"C","runs":27}

--- Problem41.java
+++ Problem41.java
@@ -9,21 +9,18 @@ public class Problem41 extends GraphicsProgram {
 
 	private GOval oval;
 	private RandomGenerator rand = RandomGenerator.getInstance();
-	
-	public void init() {
-		oval = new GOval(1, 1);
-		oval.setFilled(true);
+
+	public void run() {
+
 		addMouseListeners();
-		// oval.setColor(rand.nextColor());
-		
 
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		// GObject obj = getElementAt(e.getX(), e.getY());
-		
+		GOval oval = new GOval(1, 1);
+		oval.setFilled(true);
 		add(oval, e.getX(), e.getY());
-		
+
 	}
 
 }
