125af9834987a9be4fad249e5f28cdbf8f8dbfdf
S <s>
2023-11-11 01:38:10 +0400

    {"types":"C","runs":44}

--- mouseDragg.java
+++ mouseDragg.java
@@ -1,4 +1,36 @@
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class mouseDragg {
+import java.awt.event.MouseEvent;
 
+import acm.graphics.*;
+
+public class mouseDragg extends GraphicsProgram {
+	private GObject object;
+	private GPoint point;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void init() {
+		GRect rect = new GRect(100, 100);
+		rect.setFilled(true);
+		rect.setColor(rgen.nextColor());
+		add(rect);
+		addMouseListeners();
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+		point = new GPoint(e.getPoint());
+		object = getElementAt(point);
+
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		if (object != null) {
+			object.move(e.getX() - point.getX(), e.getY() - point.getY());
+			point = new GPoint(e.getPoint());
+		}
+		
+		
+	}
 }
