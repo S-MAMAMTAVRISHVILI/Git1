32832dfea3904d5713c98ed4ede341e6e910fad1
S <s>
2023-11-11 00:22:56 +0400

    {"types":"C","runs":5}

--- Problem41.java
+++ Problem41.java
@@ -13,6 +13,7 @@ public class Problem41 extends GraphicsProgram {
 	public void run() {
 		oval = new GOval(1, 1);
 		oval.setFilled(true);
+		add(oval, 100, 100);
 		// oval.setColor(rand.nextColor());
 		getMouseMotionListeners();
 
