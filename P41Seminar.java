40f07ce1cddae3f7b36e72df8f530590d105c8de
S <s>
2023-11-14 17:15:03 +0400

    {"types":"C","runs":31}

--- P41Seminar.java
+++ P41Seminar.java
@@ -1,4 +1,31 @@
+import java.awt.event.MouseEvent;
 
-public class P41Seminar {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class P41Seminar extends GraphicsProgram {
+	private static final int RADIUS = 20;
+	
+	
+	
+	public void run() {
+		addMouseListeners();
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		GOval brush = new GOval(2 * RADIUS, 2 * RADIUS);
+		int x = e.getX() - RADIUS;
+		int y = e.getY() - RADIUS;
+		add(brush ,x, y);
+	
+	
+	}
+	public void mousePressed(MouseEvent e) {
+		GOval brush = new GOval(2 * RADIUS, 2 * RADIUS);
+		int x = e.getX() - RADIUS;
+		int y = e.getY() - RADIUS;
+		add(brush ,x, y);
+	
+	}
+	
 }
