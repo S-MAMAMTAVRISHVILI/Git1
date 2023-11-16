b38ea3144d91f3b10902db17ad2c5239c14ccd0f
S <s>
2023-11-16 17:18:45 +0400

    {"types":"C","runs":0}

--- Strings.java
+++ Strings.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Strings {
-
+public class Strings extends ConsoleProgram {
+	public void run() {
+		String str1 = "mari";
+		String str2 = "mari";
+		String str3 =   new String("mari");
+		String str4 = readLine("enter str4:");
+		
+	
+		
+		
+		
+		
+	}
 }
