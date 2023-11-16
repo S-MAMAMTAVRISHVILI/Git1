be9cde56f0a971dfe3dbf4fc9d492e8d6c6c5896
S <s>
2023-11-16 17:31:18 +0400

    {"types":"C","runs":2}

--- Problem45.java
+++ Problem45.java
@@ -10,7 +10,7 @@ public class Problem45 extends ConsoleProgram {
 		int number = 0;
 		for(int i = 0 ;i < a.length(); i++) {
 			if(a.charAt(i) == b.charAt(i)) {
-			number += number;
+				number += number;
 			}
 		}
 		
