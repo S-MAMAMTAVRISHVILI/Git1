9f1a3c033d4fce01e0eb114956b6ebaeb4175364
S <s>
2023-11-16 17:30:43 +0400

    {"types":"C","runs":0}

--- Problem45.java
+++ Problem45.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem45 {
+public class Problem45 extends ConsoleProgram {
 
+	
+	
+	public void run() {
+		String a = readLine("Enter text:");
+		String b = readLine("Enter character:");
+		int number = 0;
+		for(int i = 0 ;i < a.length(); i++) {
+			if(a.charAt(i) == b.charAt(i)) {
+			number += number;
+			}
+		}
+		
+		
+		
+		
+	}
 }
