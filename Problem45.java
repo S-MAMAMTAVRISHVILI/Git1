cdce818a66cd5bfa2c10ce27a9b5282bdad26468
S <s>
2023-11-16 17:33:33 +0400

    {"types":"C","runs":4}

--- Problem45.java
+++ Problem45.java
@@ -5,10 +5,10 @@ public class Problem45 extends ConsoleProgram {
 	
 	
 	public void run() {
-		String a = readLine("Enter text:");
-		String b = readLine("Enter character:");
+		String a = readLine();
+		String b = readLine();
 		int number = 0;
-		for(int i = 0 ;i < a.length(); i++) {
+		for(int i = 0 ; i < a.length(); i++) {
 			if(a.charAt(i) == b.charAt(i)) {
 				number += number;
 			}
