faefd2baeb704eed9171f154d5d970910ba15aa3
S <s>
2023-11-16 17:57:44 +0400

    {"types":"C","runs":8}

--- Problem46.java
+++ Problem46.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class Problem46 {
 
+public class Problem46 extends ConsoleProgram {
+	public void run() {
+		String a = readLine("Enter text:");
+		int number = 0;
+		int max = 0;
+		for (int i = 0; i < a.length(); i++) {
+			for (int j = 0; j < a.length(); j++) {
+				if (a.charAt(i) == a.charAt(j)) {
+					number = number + 1;
+				}
+			}
+		max = number;
+//		if( > number) {
+//			
+//		}
+//		
+		number = 0;
+		
+		
+		}
+		
+
+	}
 }
