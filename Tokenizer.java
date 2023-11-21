26934e3237dbbd203b3ab8c7a5ae843c7ac77bf7
S <s>
2023-11-21 17:57:42 +0400

    {"types":"C","runs":13}

--- Tokenizer.java
+++ Tokenizer.java
@@ -1,4 +1,19 @@
+import java.util.StringTokenizer;
 
-public class Tokenizer {
+import acm.program.ConsoleProgram;
 
+public class Tokenizer extends ConsoleProgram {
+	public void run() {
+		String text  ="sandro 2 3 4";
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		
+		while(tokenizer.hasMoreTokens()) {
+			String currToken = tokenizer.nextToken();
+			println(currToken);
+			
+			
+		}
+		
+		
+	}
 }
