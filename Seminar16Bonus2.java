69ba8b2fa220dbc92cef0dac167228504615e557
S <s>
2023-11-23 01:00:45 +0400

    {"types":"C","runs":23}

--- Seminar16Bonus2.java
+++ Seminar16Bonus2.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class Seminar16Bonus2 {
-
+public class Seminar16Bonus2 extends ConsoleProgram {
+	public void run() {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
