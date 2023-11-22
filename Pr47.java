6f956fe4524c131d46481bd2dfda819e1c5bd000
S <s>
2023-11-23 01:04:53 +0400

    {"types":"C","runs":26}

--- Pr47.java
+++ Pr47.java
@@ -3,14 +3,14 @@ import acm.program.ConsoleProgram;
 public class Pr47 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text : ");
-		boolean isPalindrom = checkPalindrom(text);
-		println("The text is palindrom : " + isPalindrom);
+		boolean isPalindrome = checkPalindrome(text);
+		println("The text is palindrome : " + isPalindrome);
 		
 		
 		
 	}
 
-	private boolean checkPalindrom(String text) {
+	private boolean checkPalindrome(String text) {
 		for(int i = 0; i < text.length() / 2; i++) {
 			if(text.charAt(i) != text.charAt(text.length() - i - 1)) {
 				return false;
