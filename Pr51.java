8fbc526f6e997a075c84210e32c2511065a384f5
S <s>
2023-11-24 02:39:18 +0400

    {"types":"C","runs":37}

--- Pr51.java
+++ Pr51.java
@@ -1,4 +1,9 @@
+import acm.program.ConsoleProgram;
 
-public class Pr51 {
-
+public class Pr51 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text here: ");
+		
+	
+	}
 }
