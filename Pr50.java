3b59ce09da55ce5c64691f19cea4b846580b3485
S <s>
2023-11-24 02:35:28 +0400

    {"types":"C","runs":37}

--- Pr50.java
+++ Pr50.java
@@ -6,11 +6,7 @@ public class Pr50 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("Enter text: ");
 		StringTokenizer tokenizer = new StringTokenizer(text);
-		int number = 0;
-		while(tokenizer.hasMoreTokens()) {
-			number = number + 1;
-		}
+		int number = tokenizer.countTokens();
 		println("The text contain " + number + "words");
-		
 	}
 }
