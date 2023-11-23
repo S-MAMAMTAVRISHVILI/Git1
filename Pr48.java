f94f6c2bfa13f6818c20df076f2f295246ba8586
S <s>
2023-11-23 14:51:00 +0400

    {"types":"C","runs":1}

--- Pr48.java
+++ Pr48.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Pr48 {
-
+public class Pr48 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text : ");
+		println(text.charAt(0));
+		
+		
+		
+		
+	}
 }
