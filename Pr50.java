5439ca468513606c0a0a3c61d3b19044e89ba7af
S <s>
2023-11-24 02:28:44 +0400

    {"types":"C","runs":35}

--- Pr50.java
+++ Pr50.java
@@ -1,9 +1,12 @@
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
 public class Pr50 extends ConsoleProgram {
 	public void run() {
-		
-		
+		String text = readLine("Enter text: ");
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		print(tokenizer);
 		
 		
 	}
