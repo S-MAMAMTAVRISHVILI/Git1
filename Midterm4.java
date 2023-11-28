1a190b610e3f0afb2e6256bb1b8c23352df1f28e
S <s>
2023-11-29 02:37:07 +0400

    {"types":"C","runs":14}

--- Midterm4.java
+++ Midterm4.java
@@ -1,4 +1,25 @@
 
-public class Midterm4 {
 
+
+import java.util.StringTokenizer; 
+import acm.program.ConsoleProgram;
+
+
+
+public class Midterm4 extends ConsoleProgram {
+	public void run() {
+		String s = readLine("Enter sentence : ");
+		String sentence = "";
+		StringTokenizer tokenizer = new StringTokenizer(s);
+		while(tokenizer.hasMoreTokens()) {
+			String word = tokenizer.nextToken();
+			String reversedWord = "";
+			for(int i = 0; i < word.length(); i++) {
+				String x = Character.toString(word.charAt(word.length() - i - 1));
+				reversedWord = reversedWord + x;
+			}
+			sentence = sentence + reversedWord + " ";
+		}
+		println(sentence);
+	}
 }
