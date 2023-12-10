b925bdcb6fbd338eaf6fea36e5c598f1668d4961
S <s>
2023-12-10 21:28:26 +0400

    {"types":"C","runs":3}

--- Problem52.java
+++ Problem52.java
@@ -11,12 +11,12 @@ public class Problem52 extends ConsoleProgram {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("Problem52.java"));
 			while(true) {
-			if(reader.readLine() == null) {
-				break;
-			} else {
 				String newLine = reader.readLine();
+				if(reader.readLine() == null) {
+					break;
+				} 
 				println(newLine);
-			}
+				
 			}
 			reader.close();
 		} catch (FileNotFoundException e) {
