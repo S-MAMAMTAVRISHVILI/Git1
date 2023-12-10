ddb093847c838dd1e6a9ff1f6695c8d99407e3df
S <s>
2023-12-10 21:53:57 +0400

    {"types":"C","runs":8}

--- count.java
+++ count.java
@@ -1,4 +1,14 @@
+import java.util.StringTokenizer;
 
-public class count {
+import acm.program.ConsoleProgram;
 
+public class count extends ConsoleProgram {
+	private int SYMBOL_NUMBER = 0;
+	public void run() {
+		String newLine = "Belli BelliGoal Bellighhhhh";
+		StringTokenizer tokenizer = new StringTokenizer(newLine);
+		String st = tokenizer.toString();
+		println(st);
+		SYMBOL_NUMBER += st.length(); 
+	}
 }
