f05239e4a30ccf4744dd6dc3c6c551b33f9b49f6
S <s>
2023-12-10 21:26:03 +0400

    {"types":"C","runs":2}

--- Problem52.java
+++ Problem52.java
@@ -1,4 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Problem52 {
+import acm.program.ConsoleProgram;
 
+public class Problem52 extends ConsoleProgram {
+	
+	public void run() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("Problem52.java"));
+			while(true) {
+			if(reader.readLine() == null) {
+				break;
+			} else {
+				String newLine = reader.readLine();
+				println(newLine);
+			}
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
