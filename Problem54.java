f86186ded848a1e7917e73cac490bc3a8546a244
S <s>
2023-12-11 19:28:11 +0400

    {"types":"C","runs":0}

--- Problem54.java
+++ Problem54.java
@@ -1,4 +1,17 @@
+import java.util.Arrays;
 
-public class Problem54 {
+import acm.program.ConsoleProgram;
 
+public class Problem54 extends ConsoleProgram {
+	public void run() {
+		int n = readInt();
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++) {
+			array[i] = readInt();
+		}
+		Arrays.sort(array);
+		println(array[n - 2]);
+		println(array[n - 1]);
+	}
 }
+ 
