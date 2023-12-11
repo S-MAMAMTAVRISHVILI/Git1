c71bff5ca03b94bc531026d02d61ae3df9bbdf29
S <s>
2023-12-11 19:36:04 +0400

    {"types":"C","runs":3}

--- Problem55.java
+++ Problem55.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
 
-public class Problem55 {
+import acm.program.ConsoleProgram;
 
+public class Problem55 extends ConsoleProgram {
+	
+	public void run() {
+		int n = readInt();
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++) {
+			array[i] = readInt();
+		}
+		int[] arrayOld = array;
+		Arrays.sort(array);
+		if(array.equals(arrayOld)) {
+			println("sorted");
+		} else {
+			println("not sorted");
+		}
+	}
 }
