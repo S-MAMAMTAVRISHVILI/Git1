375e719a8ab0cfdef9bcc33ce79f792e0e8cb343
S <s>
2023-12-12 18:02:10 +0400

    {"types":"C","runs":5}

--- Problem58.java
+++ Problem58.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem58 {
-
+public class Problem58 extends ConsoleProgram{
+	public void run() {
+		int[][] matrix = new int[20][20];
+		println(magicMatrix(matrix));
+	}
+	private boolean magicMatrix(int[][] matrix) {
+		if(matrix[0].length != matrix.length) {
+			return false;
+		}
+		for(int i = 0; i < matrix.length; i++) {
+			
+		}
+		
+		
+		return false;
+		
+	}
+	
 }
