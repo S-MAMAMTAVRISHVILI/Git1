5c16f8a4016f397b5a122e425cdcb77bbc179953
S <s>
2023-12-12 17:45:50 +0400

    {"types":"C","runs":4}

--- MatrixPractice.java
+++ MatrixPractice.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class MatrixPractice {
+public class MatrixPractice extends ConsoleProgram {
+	public void run() {
+		matrixSample();
+//		arraySample();
+//		arrayListSample();
+	}
 
+	private void matrixSample() {
+		int[][] myMatrix = new int[10][5];
+		println(myMatrix);
+		for (int i = 0; i < myMatrix.length; i++) {
+			println(myMatrix[i]);
+			for(int j = 0; j < myMatrix[i].length; i++) {
+				println(myMatrix[i][j]);
+			}
+		}
+	}
 }
