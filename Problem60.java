b083125d5cf2e9d5bd0d5c2f28490d11302d76be
S <s>
2023-12-12 17:32:15 +0400

    {"types":"C","runs":3}

--- Problem60.java
+++ Problem60.java
@@ -13,7 +13,7 @@ public class Problem60 extends ConsoleProgram {
 			list.add(digit);
 		}
 		println("Reversed : ");
-		for(int i = list.size(); i > 0; i--) {
+		for(int i = list.size() - 1; i >= 0; i--) {
 			println(list.get(i));
 		}
 	
