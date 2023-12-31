c7a71787fc2b40a8c0de130739e26ac5fe832b73
S <s>
2023-12-31 21:49:28 +0400

    {"types":"C","runs":6}

--- Pr61.java
+++ Pr61.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
 
-public class Pr61 {
+import acm.program.ConsoleProgram;
 
+public class Pr61 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("n : ");
+		int m = readInt("m : ");
+		ArrayList<Integer> numbersList = new ArrayList<Integer>();
+		for(int i = 0; i < n; i++) {
+			int x = readInt();
+			numbersList.add(x);
+		}
+		for(int i = 0; i < numbersList.size(); i++) {
+			for(int j = 0; j < numbersList.size(); j++) {
+				if(numbersList.get(i) + numbersList.get(j) == m) {
+					println(numbersList.get(i) + " and " + numbersList.get(j));
+				}
+			}
+		}
+	}
 }
