694560b9191f022ed73293b5eef97d61144a48f3
S <s>
2023-12-31 21:33:10 +0400

    {"types":"C","runs":2}

--- Pr62.java
+++ Pr62.java
@@ -1,4 +1,26 @@
+import java.util.HashMap;
 
-public class Pr62 {
+import acm.program.ConsoleProgram;
 
+public class Pr62 extends ConsoleProgram {
+	public void run() {
+		HashMap<String, Integer> nameBase = new HashMap<String, Integer>();
+		while(true) {
+			String name = readLine("Enter name");
+			if(name.equals("")) {
+				break;
+			}
+			if(!nameBase.containsKey(name)) {
+				nameBase.put(name, 1);
+			} else {
+				int x = nameBase.get(name);
+				nameBase.put(name, x + 1);
+			}
+		}
+		for(String name: nameBase.keySet()) {
+			println(name + " " + nameBase.get(name));
+		}
+	}
+	
+	
 }
