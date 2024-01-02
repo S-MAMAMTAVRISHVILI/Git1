4e1f02db3b13d73d4e2dd3c2054ca173380cf3f3
S <s>
2024-01-02 16:13:18 +0400

    {"types":"C","runs":2}

--- Pr63.java
+++ Pr63.java
@@ -1,7 +1,21 @@
+import java.util.HashMap;
+
 import acm.program.ConsoleProgram;
 
 public class Pr63 extends ConsoleProgram {
 	public void run() {
+		HashMap<String, String> followersList = new HashMap<String, String>();
+		while(true) {
+			String newFollower = readLine();
+			if(newFollower.equals("")) {
+				break;
+			}
+			String person = readLine();
+			if(person.equals("")) {
+				break;
+			}
+			followersList.put(person, newFollower);
+		}
 		
 	}
 }
