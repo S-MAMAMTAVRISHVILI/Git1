62a2322241025e660e915816f94d9f8d9b6aee8a
S <s>
2024-01-02 17:41:29 +0400

    {"types":"C","runs":5}

--- Pr64.java
+++ Pr64.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
 public class Pr64 extends ConsoleProgram {
-
+	public void run() {
+		HashMap<String, ArrayList<String>> friendsList = new HashMap<String, ArrayList<String>>();
+		while(true) {
+			String line = readLine();
+			if(line.equals("")) {
+				break;
+			}
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			String person = tokenizer.nextToken();
+			String friend = tokenizer.nextToken();
+			friendsList.putIfAbsent(person, new ArrayList<String>());
+			friendsList.get(person).add(friend);
+		}
+		for(String name : friendsList.keySet()) {
+			println(name + " " + friendsList.get(name).toString());
+		}
+	}
 }
